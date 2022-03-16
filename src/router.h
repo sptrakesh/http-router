b7574a6e1576ef00d22a8c68a6437e509fb6fd80
Rakesh Vidyadharan <rakesh@sptci.com>
2022-03-16 12:29:07 -0500

    Minor refactor.

--- src/router.h
+++ src/router.h
@@ -23,8 +23,8 @@ namespace spt::http::router
         handler{ h } {}
 
       ~Path() = default;
-      Path(Path&&) = default;
-      Path& operator=(Path&&) = default;
+      Path(Path&&) noexcept = default;
+      Path& operator=(Path&&) noexcept = default;
       Path(const Path&) = delete;
       Path& operator=(const Path&) = delete;
 
@@ -120,8 +120,6 @@ namespace spt::http::router
           }
           if ( iter->parts[i][0] != '{' ) break;
 
-          auto key = std::string{ iter->parts[i].substr( 1, iter->parts[i].size() - 2 ) };
-          if ( key == full ) {}
           params[iter->parts[i].substr( 1, iter->parts[i].size() - 2 )] = parts[i];
           handler = iter->handler;
         }
@@ -134,113 +132,4 @@ namespace spt::http::router
     std::vector<Handler> handlers{};
     std::vector<Path> paths;
   };
-
-  /*
-  template<typename UserData, typename Response>
-  class HttpRouter
-  {
-  private:
-    struct Node
-    {
-      using Ptr = std::unique_ptr<Node>;
-      Node( std::string n, short h ) : name{ std::move( n ) }, handler{ h } {}
-      std::string name;
-      std::unordered_map<std::string, Node::Ptr> children{};
-      short handler;
-    };
-
-  public:
-    using Handler = std::function<Response( UserData, std::unordered_map<std::string, std::string>&& )>;
-
-    HttpRouter()
-    {
-      handlers.reserve( 16 );
-    }
-
-    HttpRouter& add( std::string_view method, std::string_view path, Handler&& handler )
-    {
-      using namespace std::string_view_literals;
-
-      handlers.push_back( std::move( handler ) );
-      const auto parts = util::split( path, 8, "/"sv );
-
-      if ( !tree ) tree = std::make_unique<Node>( std::string{ method }, -1 );
-
-      add( method, parts, handlers.size() );
-      handlers.push_back( std::move( handler ) );
-      return *this;
-    }
-
-    std::optional<Response> route( std::string_view method, std::string_view path, UserData userData )
-    {
-      using namespace std::string_view_literals;
-
-      if ( method.empty() || path.empty() ) return std::nullopt;
-      if ( !tree ) return std::nullopt;
-
-      const std::string m{ method };
-      if ( !tree->children.contains( m ) ) return std::nullopt;
-
-      std::unordered_map<std::string, std::string> params{};
-
-      auto parent = tree->children[m].get();
-      const auto parts = util::split( path, 8, "/"sv );
-      short handler{ -1 };
-      for ( auto&& part : parts )
-      {
-        if ( part.empty() ) return std::nullopt;
-        if ( !parent ) return std::nullopt;
-
-        const std::string p{ part };
-        if ( !parent->children.contains( p ) )
-        {
-          bool found{ false };
-          for ( auto&& pair : parent->children )
-          {
-            if ( pair.first[0] != ':' ) continue;
-            const auto name = std::string{ std::string_view{ pair.first }.substr( 1 ) };
-            params[name] = p;
-            handler = parent->handler;
-            found = true;
-            parent = pair.second.get();
-            break;
-          }
-          if ( !found ) return std::nullopt;
-        }
-        else
-        {
-          handler = parent->handler;
-          parent = parent->children[p].get();
-        }
-      }
-
-      if ( handler < 0 ) return std::nullopt;
-      return handlers[handler]( userData, std::move( params ) );
-    }
-
-  private:
-    void add( std::string_view method, const std::vector<std::string_view>& parts, short handler )
-    {
-      Node* parent = tree.get();
-      const std::string m{ method };
-      if ( !parent->children.contains( m ) ) parent->children[m] = std::make_unique<Node>( m, handler );
-      parent = parent->children[m].get();
-
-      for ( auto&& part: parts )
-      {
-        std::string name{ part };
-        if ( !parent->children.contains( name ) )
-        {
-          parent->children[name] = std::make_unique<Node>( name, handler );
-        }
-        parent = parent->children[name].get();
-      }
-
-      parent->handler = handler;
-    }
-
-    std::vector<Handler> handlers{};
-    typename Node::Ptr tree{ nullptr };
-  };
-   */
 }
